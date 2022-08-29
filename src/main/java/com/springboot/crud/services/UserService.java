58e05ef8645e5ba87ab47f197cb7e6083561905e
Gabriel Pereira <gabriel.psouza1997@outlook.com>
2022-08-29 20:08:15 -0300

    Insercao de usuario com post

--- src/main/java/com/springboot/crud/services/UserService.java
+++ src/main/java/com/springboot/crud/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.crud.domain.User;
+import com.springboot.crud.dto.UserDto;
 import com.springboot.crud.repository.UserRepository;
 import com.springboot.crud.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 	 Optional<User> user = userRepository.findById(id);
 	 return user.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+	public User insert(User user) {
+		return userRepository.save(user);
+	}
+	
+	public User fromDto(UserDto obj) {
+		return new User(obj.getId(),obj.getName(),obj.getEmail());
 	}
 	
 	
